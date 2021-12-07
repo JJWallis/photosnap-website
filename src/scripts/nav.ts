267062ba3cbfbec67761381f0f4a329f0c233737
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-07 21:57:22 +0000

    Hamburger ip

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,14 +1,17 @@
 const body = document.querySelector('body')
-const hamburger = body.querySelector('#btn-hamburger')
+const hamburgerBtn = body.querySelector('#btn-hamburger')
+const hamburger = body.querySelector('.hamburger')
 const nav = body.querySelector('.nav-mobile--container')
 
-hamburger.addEventListener('click', () => {
+hamburgerBtn.addEventListener('click', () => {
    const isVisible = nav.getAttribute('data-visible')
    if (isVisible === 'true') {
       nav.setAttribute('data-visible', 'false')
       body.setAttribute('data-visible', 'false')
+      hamburger.setAttribute('data-visible', 'false')
    } else {
       nav.setAttribute('data-visible', 'true')
       body.setAttribute('data-visible', 'true')
+      hamburger.setAttribute('data-visible', 'true')
    }
 })
