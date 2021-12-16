0a107295ec4d34b768e986319bee20b8af2c1131
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-16 21:16:07 +0000

    JS pricing logic ip

--- src/scripts/prices.ts
+++ src/scripts/prices.ts
@@ -1,5 +1,6 @@
 const toggle: HTMLInputElement = document.querySelector('#toggle')
 const amounts = document.querySelectorAll('.pricing-card__price__amount')
+const timeFrames = document.querySelectorAll('.pricing-card__price__timeframe')
 
 toggle.addEventListener('change', () => {
    if (toggle.checked) {
@@ -8,6 +9,8 @@ toggle.addEventListener('change', () => {
          value.splice(3, 0, '0')
          amount.textContent = value.join('')
       })
+
+      timeFrames.forEach((timeFrame) => (timeFrame.textContent = 'year'))
       return
    }
 
@@ -16,4 +19,6 @@ toggle.addEventListener('change', () => {
       value.splice(3, 1, '')
       amount.textContent = value.join('')
    })
+
+   timeFrames.forEach((timeFrame) => (timeFrame.textContent = 'month'))
 })
