3c9a6ba8124b6324a020a0cb8c3b27eea6fed83a
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-16 21:25:46 +0000

    JS pricing logic done

--- src/scripts/prices.ts
+++ src/scripts/prices.ts
@@ -1,9 +1,15 @@
 const toggle: HTMLInputElement = document.querySelector('#toggle')
+const labelMonthly = document.querySelector('#label-monthly')
+const labelYearly = document.querySelector('#label-yearly')
 const amounts = document.querySelectorAll('.pricing-card__price__amount')
 const timeFrames = document.querySelectorAll('.pricing-card__price__timeframe')
+const classList = (el, action, classN) => {}
 
 toggle.addEventListener('change', () => {
    if (toggle.checked) {
+      labelMonthly.classList.remove('active')
+      labelYearly.classList.add('active')
+
       amounts.forEach((amount) => {
          const value = amount.textContent.split('')
          value.splice(3, 0, '0')
@@ -13,6 +19,9 @@ toggle.addEventListener('change', () => {
       timeFrames.forEach((timeFrame) => (timeFrame.textContent = 'year'))
       return
    }
+
+   labelMonthly.classList.add('active')
+   labelYearly.classList.remove('active')
 
    amounts.forEach((amount) => {
       const value = amount.textContent.split('')
